a05ad8b66d35338b9b1c5db3008e9a21611bd305
zhanzif <zhanzif@worldunion.com.cn>
2020-01-14 18:37:49 +0800

    eureka commit

--- canon-platform/src/main/java/com/canon/service/impl/IdCenterServiceImpl.java
+++ canon-platform/src/main/java/com/canon/service/impl/IdCenterServiceImpl.java
@@ -4,9 +4,13 @@ import com.canon.dao.IdCenterMapper;
 import com.canon.platform.model.IdCenter;
 import com.canon.service.IIdCenterService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * @program: canon-dependencies
@@ -18,7 +22,10 @@ import javax.annotation.Resource;
 public class IdCenterServiceImpl implements IIdCenterService {
 
     @Resource
-    IdCenterMapper idCenterMapper;
+    private IdCenterMapper idCenterMapper;
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public IdCenter findById(Long id) {
@@ -26,7 +33,12 @@ public class IdCenterServiceImpl implements IIdCenterService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public void add(IdCenter idCenter) {
-
+        String sql = "INSERT INTO aptbu.t_idcenter (id, app, tableName, cindex, createtime, updatetime, keypre, fstep, dbid) value (?,?,?,?,?,?,?,?,?)";
+        LocalDateTime now = LocalDateTime.now();
+        Object[] params = new Object[] {10000,"test-app","test-table",100,now, System.currentTimeMillis(),null,50,10000L};
+        jdbcTemplate.update(sql, params);
+        // int count = 1/0;
     }
 }
